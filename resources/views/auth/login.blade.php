5bd6ae43890e27681739b0c5d5f0dae2d22789f2
kenny4 <kennygiets@hotmail.com>
2018-10-18 14:44:42 +0200

    Edited login view

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,59 +8,50 @@
                     <form method="POST" action="{{ route('login') }}" class="mx-auto bg-white">
                         @csrf
                         <h2 class="text-center">Vaccinbeheer</h2>
-                        <div class="form-group row">
-                            <label for="email" class="col-sm-4 col-form-label text-md-right">Email:</label>
+                        <div class="form-group">
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
+                                <input id="email" placeholder="Email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
 
                                 @if ($errors->has('email'))
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $errors->first('email') }}</strong>
                                     </span>
                                 @endif
-                            </div>
                         </div>
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">Wachtwoord:</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+                        <div class="form-group">
+                                <input id="password" placeholder="Wachtwoord" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
 
                                 @if ($errors->has('password'))
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $errors->first('password') }}</strong>
                                     </span>
                                 @endif
-                            </div>
                         </div>
 
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
+                        <div class="form-group">
+                                <button type="submit" class="btn btn-dark btn-block form-control">
+                                    Inloggen
+                                </button>
+                        </div>
+                        <div class="col-md-8 offset-md-2">
+                            <a class="btn btn-link" href="{{ route('password.request') }}">
+                                Wachtwoord vergeten?
+                            </a>
+                        </div>
+                    </form>
+                </div>
+            </div>
+@endsection
+
+                    <!--
+                        <div class="form-group">
                                 <div class="form-check">
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
                                     <label class="form-check-label" for="remember">
                                         Onthoud mij
                                     </label>
-                                </div>
                             </div>
                         </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-                            </div>
-                            <div class="col-md-8 offset-md-2">
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    Wachtwoord vergeten?
-                                </a>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-@endsection
+                    -->
--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -15,7 +15,7 @@
 
     <!-- NAV BAR -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <a class="navbar-brand" href="#">Vaccinbeheer</a>
+            <a class="navbar-brand" href="">Vaccinbeheer</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExample05"
                 aria-controls="navbarsExample05" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
